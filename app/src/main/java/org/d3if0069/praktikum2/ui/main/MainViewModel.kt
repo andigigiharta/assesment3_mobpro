94aabc97db14210c3eb144e7fdb6ad83942cc049
Andi Gigih Artadwiputra <andigigihartadwiputra.github@domain.com>
2023-05-24 14:11:03 +0700

    Mengambil data dari internet

--- app/src/main/java/org/d3if0069/praktikum2/ui/main/MainViewModel.kt
+++ app/src/main/java/org/d3if0069/praktikum2/ui/main/MainViewModel.kt
@@ -1,16 +1,34 @@
 package org.d3if0069.praktikum2.ui.main
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.d3if0069.praktikum2.Hewan
 import org.d3if0069.praktikum2.R
+import org.d3if0069.praktikum2.network.HewanApi
 
 class MainViewModel : ViewModel() {
     private val data = MutableLiveData<List<Hewan>>()
     init {
         data.value = initData()
+        retrieveData()
+
     }
+    private fun retrieveData() {
+        viewModelScope.launch (Dispatchers.IO) {
+            try {
+                val result = HewanApi.service.getHewan()
+                Log.d("MainViewModel", "Success: $result")
+            } catch (e: Exception) {
+                Log.d("MainViewModel", "Failure: ${e.message}")
+            }
+        }
+    }
+
     // Data ini akan kita ambil dari server di langkah selanjutnya
     private fun initData(): List<Hewan> {
         return listOf(
